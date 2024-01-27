e2a385f8c081c7c8c28b6fd28f1f4013426c5370
Cassidy Williams <1454517+cassidoo@users.noreply.github.com>
2024-01-26 23:50:57 -0600

    Add local storage and tab logic

--- craft.js
+++ craft.js
@@ -2,6 +2,23 @@ const { degrees, PDFDocument, rgb } = PDFLib;
 
 let pathUrl = "";
 
+(function () {
+	checkForPath();
+	document.getElementById("file").addEventListener("change", () => {
+		pathUrl = document.getElementById("file").files[0].path;
+	});
+})();
+
+function checkForPath() {
+	if (localStorage.getItem("path")) {
+		pathUrl = localStorage.getItem("path");
+	}
+}
+
+function savePath() {
+	localStorage.setItem("path", pathUrl);
+}
+
 async function modifyPdf() {
 	const url = pathUrl;
 	const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
@@ -28,8 +45,20 @@ async function modifyPdf() {
 	download(pdfBytes, `w9-${localDate.replaceAll("/", "-")}`, "application/pdf");
 }
 
-function getPath() {
-	let path = document.getElementById("file").files[0].path;
-	document.getElementById("path").innerHTML = path;
-	pathUrl = path;
+function openTab(event, tabName) {
+	let i, tabcontent, tablinks;
+
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].classList.remove("active");
+	}
+
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+		tablinks[i].classList.remove("active");
+	}
+
+	document.getElementById(tabName).classList.add("active");
+	event.currentTarget.classList.add("active");
 }
